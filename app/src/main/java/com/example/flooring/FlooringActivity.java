f70f7af7a88f42ef82522058bed39685def457f5
jackseibert <jackwseibert@gmail.com>
2019-02-28 18:13:33 -0600

    Initial commit for Asg 7 - NOT DONE

---
+++ app/src/main/java/com/example/flooring/FlooringActivity.java
@@ -0,0 +1,49 @@
+package com.example.flooring;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FlooringActivity extends AppCompatActivity {
+
+    TextView tvResult;
+    EditText etWidth;
+    EditText etLength;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flooring);
+
+        tvResult = (TextView) findViewById(R.id.textViewResult);
+        etWidth = (EditText) findViewById(R.id.editTextWidth);
+        etLength = (EditText) findViewById(R.id.editTextLength);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent i = getIntent();
+        uriString = i.getStringExtra("text_label");
+
+        /*Bundle extras = getIntent().getExtras();
+        tvResult.setText("Width is " + (etWidth.getText().toString()) + " and Length is " + (etLength.getText().toString())
+        + " and flooring needed is ");*/
+
+    }
+
+}
---
+++ app/src/main/java/com/example/flooring/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.flooring;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etWidth;
+    EditText etLength;
+    Button calcButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etWidth = (EditText) findViewById(R.id.editTextWidth);
+        etLength = (EditText) findViewById(R.id.editTextLength);
+
+        calcButton = (Button) findViewById(R.id.buttonCalc);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+
+                // Call the flooring activity using an intent
+                Intent detailActIntent = new Intent(v.getContext(), FlooringActivity.class);
+                detailActIntent.putExtra("etWidth", etWidth.getText().toString());
+                //double width = Double.parseDouble(etWidth.getText().toString());
+                detailActIntent.putExtra("etLength", etLength.getText());
+                //double length = Double.parseDouble(etLength.getText().toString());
+                startActivity(detailActIntent);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
